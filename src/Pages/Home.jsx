0e3cf5cde91652e573963988c5ccc9cff8499df1
Rowan Williams <rowan@reveal.co>
2025-04-10 12:09:36 +0200

    finalise home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./Home.css";
 import NewsBlock from "../Components/NewsBlock";
-import { supporting, beyondReach } from "../Images/BreakingForm/breakingFormExport";
-import { marchSeems, outTake } from "../Images/dancer/dancerExport";
+import { beyondReach } from "../Images/BreakingForm/breakingFormExport";
+import { marchSeems} from "../Images/dancer/dancerExport";
+import { musicAndBeing1, theLyric} from "../Images/work/workExport";
 import Credits from "../Components/Credits";
 import Footer from "../Components/Footer";
-// import { film } from "../Images/about/aboutExport";
 
 const Home = () => {
   const backgroundImage = {
@@ -29,19 +29,56 @@ const Home = () => {
     </>
   );
   
-  const boxContent = (
+  const musicBeingContent = (
     <>
         <p>
-            I am a creative who works well with others. I have an experienced and
-            critical eye for movement as well as connection with silence, sound and
-            music having worked within a professional capacity both as a dancer and
-            choreographer. I have spent the last thirty years as teacher of Actor
-            Movement at Royal Central School of Speech & Drama, including
-            co-authoring <i>Actor Movement: Expression of the Physical Being</i>,
-            Bloomsbury, 2015.
+        Launch of Music and Being Collective, 09.02.23 at LUMA presented by AORA.
+        Here as Meditation Leader in About A Place – a guided musical mediation by Alex Mills (composer).
+        More information can be found via the {" "}
+          <a href="https://issuu.com/royalphotographicsociety/docs/june_2023_we_are_magazine_final_23-_ff483e95440cdb?fr=sMTZjYTU5NTAxNDg">
+            Royal Photographic Society
+          </a>
+        {" "} on page 30
         </p>
     </>
   )
+
+  const breakingFormContent = (
+    <>
+        <p>
+          A photographic project and unique collaboration between three people:
+          Breaking Form: Buz and Parkinson’s was developed over two decades by Buz
+          Williams, his wife, actor movement specialist Debbie Green, and
+          photographer Clare Park.
+        </p>
+        <p>
+        <b>
+          Buy now - email: clareparkphoto@hotmail.com - you can pay by cheque or
+          bank transfer
+        </b>
+      </p>
+      <p>Price: £18.99 (excluding postage)</p>
+    </>
+  )
+
+  const theLyricContent = (
+    <>
+        <p>
+        The Lyric “I”, 2021, is a narrative photographic series by Clare and
+        I, that interprets/re-images Anne Gruenberg’s poetry. I perform ‘The
+        Dancer’ who portrays the desolation and reawakening from Anne’s mental
+        breakdown over a 12year period in secure institutions. For Clare and I
+        The Dancer is the expressive conduit to Anne’s work; Anne’s creativity
+        as a theatre designer was rekindled with the idea of The Dancer as
+        protagonist. The Dancer wears the ‘theatrical/archetypal’ red dress
+        Anne sourced; she moves in different landscapes with different props.
+        It is this combination that creates the heightened reality, symbolism
+        and spirituality redolent in each colour image.
+      </p>
+    </>
+  )
+
+
 
   return (
     <div className="home-main">
@@ -55,22 +92,23 @@ const Home = () => {
       />
       <div className="home-content">
         <div className="home-written">
+        <h2>Latest Projects</h2>
             <NewsBlock 
-                title="Testing Title"
-                pictureSrc={outTake}
-                content={boxContent}
+                title="Music and Being Collective"
+                pictureSrc={musicAndBeing1}
+                content={musicBeingContent}
 
             />
             <NewsBlock 
-                title="Testing Title"
-                pictureSrc={supporting}
-                content={boxContent}
+                title="Breaking Form: Re-Formed"
+                pictureSrc={beyondReach}
+                content={breakingFormContent}
 
             />
             <NewsBlock 
-                title="Testing Title"
-                pictureSrc={marchSeems}
-                content={boxContent}
+                title="The Lyric"
+                pictureSrc={theLyric}
+                content={theLyricContent}
 
             />
 
